0fb08a24ac5c19f547a6420e66053b5c858e1da0
sebastian <sebastianbobrinsky@gmail.com>
2019-06-02 15:23:47 -0400

    next

--- src/main/java/strings/Solution212.java
+++ src/main/java/strings/Solution212.java
@@ -3,6 +3,69 @@ package strings;
 import java.util.*;
 
 public class Solution212 {
+
+    /**
+     * This solution was accepted, but the task Hint 2 suggests to use Trie.
+     */
+
+    public List<String> findWords(char[][] board, String[] words) {
+        List<String> res = new ArrayList<>();
+        for (String word: words) {
+            int[][] excluded = new int[board.length][board[0].length];
+            Set<Character> dict = new HashSet<>();
+            for (Character ch: word.toCharArray())
+                dict.add(ch);
+            for (int i = 0; i < board.length; i++) {
+                boolean found = false;
+                for (int j = 0; j < board[0].length; j++) {
+                    if (excluded[i][j] == 0) {
+                        found = findWord(board, word, i, j, 0, new StringBuilder(), excluded, dict);
+                        if (found) {
+                            res.add(word);
+                            break;
+                        }
+                    }
+                }
+                if (found)
+                    break;
+            }
+        }
+        return res;
+    }
+
+    private boolean findWord(char[][] board, String word, int i, int j, int k, StringBuilder buf, int[][] excluded, Set<Character> dict) {
+        if (word.equals(buf.toString()))
+            return true;
+        if (i < 0 || i == board.length || j < 0 || j == board[0].length)
+            return false;
+
+        if (excluded[i][j] == 1 || excluded[i][j] == 2) {
+            return false;
+        }
+
+        if (!dict.contains(board[i][j])) {
+            excluded[i][j] = 1;
+            return false;
+        }
+
+        if (board[i][j] == word.charAt(k)) {
+            buf.append(word.charAt(k));
+            int pathValue = excluded[i][j];
+            excluded[i][j] = 2;
+            boolean res = findWord(board, word, i + 1, j, k + 1, buf, excluded, dict) ||
+                    findWord(board, word, i - 1, j, k + 1, buf, excluded, dict) ||
+                    findWord(board, word, i, j + 1, k + 1, buf, excluded, dict) ||
+                    findWord(board, word, i, j - 1, k + 1, buf, excluded, dict);
+            buf.deleteCharAt(buf.length() - 1);
+            excluded[i][j] = pathValue;
+            return res;
+        }
+        return false;
+    }
+
+    /**
+     * This solution was too slow and it met Time Limit Exceeded error
+
     public List<String> findWords(char[][] board, String[] words) {
         List<String> res = new ArrayList<>();
         for (String word: words) {
@@ -29,6 +92,7 @@ public class Solution212 {
             return false;
         if (path.contains(i + "" + j))
             return false;
+
         if (board[i][j] == word.charAt(k)) {
             buf.append(word.charAt(k));
             path.add(i + "" + j);
@@ -42,4 +106,5 @@ public class Solution212 {
         }
         return false;
     }
+     */
 }
